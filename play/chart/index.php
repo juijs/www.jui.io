26ba7f651641de6aba4b0925c16072f9ae452da1
seogi1004 <seogi777@gmail.com>
2015-12-31 15:02:09 +0900

    UI & Chart Play: 메뉴 닫기 터치 이벤트로 변경

--- play/chart/index.php
+++ play/chart/index.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="chart.css">
     <link rel="stylesheet" href="responsive.css">
     <script src="chart.js" type="text/javascript"></script>
+    <script src="event.js" type="text/javascript"></script>
 </head>
 <body class="jui">
 
