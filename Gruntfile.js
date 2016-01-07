5dc5f6884ae302ccf90b32614ed43018a3fd77f2
seogi1004 <seogi777@gmail.com>
2016-01-07 17:44:33 +0900

    1. JUI 프로젝트 동기화 2. JUI 프로젝트 동기화 그런트 모듈 추가

---
+++ Gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt) {
+	var webapp_path = "lib/";
+
+	grunt.initConfig({
+		"curl-dir": {
+			"jui-core.js": {
+				src: [
+					"https://raw.githubusercontent.com/juijs/jui-core/develop/dist/core.js",
+					"https://raw.githubusercontent.com/juijs/jui-core/develop/dist/core.min.js"
+				],
+				dest: webapp_path + "jui/js"
+			},
+			"jui-chart.js": {
+				src: [
+					"https://raw.githubusercontent.com/juijs/jui-chart/develop/dist/chart.js",
+					"https://raw.githubusercontent.com/juijs/jui-chart/develop/dist/chart.min.js"
+				],
+				dest: webapp_path + "jui/js"
+			},
+			"jui.js": {
+				src: [
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui.js",
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui.min.js"
+				],
+				dest: webapp_path + "jui/js"
+			},
+			"jui.css": {
+				src: [
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui.css",
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui.min.css",
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui-jennifer.css",
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui-jennifer.min.css",
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui-dark.css",
+					"https://raw.githubusercontent.com/juijs/jui/develop/dist/ui-dark.min.css"
+				],
+				dest: webapp_path + "jui/css"
+			},
+			"jui-grid.js": {
+				src: [
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid.js",
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid.min.js"
+				],
+				dest: webapp_path + "jui/js"
+			},
+			"jui-grid.css": {
+				src: [
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid.css",
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid.min.css",
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid-jennifer.css",
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid-jennifer.min.css",
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid-dark.css",
+					"https://raw.githubusercontent.com/juijs/jui-grid/develop/dist/grid-dark.min.css"
+				],
+				dest: webapp_path + "jui/css"
+			}
+		},
+
+		pkg: grunt.file.readJSON("package.json")
+	});
+
+	require("load-grunt-tasks")(grunt);
+};
+
