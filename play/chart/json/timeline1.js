944c371ef491eb8d8c0379744b59f10bac46c5ad
seogi1004 <seogi777@gmail.com>
2016-01-20 17:11:55 +0900

    1. JUI library updated 2. Upgrade timeline chart #12

--- play/chart/json/timeline1.js
+++ play/chart/json/timeline1.js
@@ -1,56 +1,44 @@
 var builder = jui.include("chart.builder");
 
-// 'type, stime, etime' properties are required!!!
+// 'key, stime, etime' properties are required!!!
 var data = [
-    { type: "http://google.co.kr/", stime: 0, etime: 612, kind: "dns" },
-    { type: "http://google.co.kr/", stime: 612, etime: 613, kind: "connect" },
-    { type: "http://google.co.kr/", stime: 613, etime: 630, kind: "wait" },
-
-    { type: "http://www.google.co.kr/", stime: 630, etime: 640, kind: "dns" },
-    { type: "http://www.google.co.kr/", stime: 640, etime: 641, kind: "connect" },
-    { type: "http://www.google.co.kr/", stime: 641, etime: 660, kind: "wait" },
-
-    { type: "https://www.google.co.kr/?gws_rd=ssl", stime: 660, etime: 706, kind: "ssl" },
-    { type: "https://www.google.co.kr/?gws_rd=ssl", stime: 706, etime: 753, kind: "connect" },
-    { type: "https://www.google.co.kr/?gws_rd=ssl", stime: 753, etime: 810, kind: "wait" },
-    { type: "https://www.google.co.kr/?gws_rd=ssl", stime: 810, etime: 813, kind: "receive" },
-
-    { type: "nav_logo242.png", stime: 813, etime: 829, kind: "wait" },
-
-    { type: "googlelogo_color_272x92dp.png", stime: 829, etime: 845, kind: "wait" },
-
-    { type: "i1_1967ca6a.png", stime: 845, etime: 856, kind: "ssl" },
-    { type: "i1_1967ca6a.png", stime: 856, etime: 869, kind: "connect" },
-    { type: "i1_1967ca6a.png", stime: 869, etime: 871, kind: "wait" },
-
-    { type: "rs=ACT90oHKONBv_Rd-Dj71NZEExnlU9sHrEg", stime: 871, etime: 874, kind: "wait" },
-    { type: "rs=ACT90oHKONBv_Rd-Dj71NZEExnlU9sHrEg", stime: 874, etime: 878, kind: "receive" },
-
-    { type: "tia.png", stime: 878, etime: 884, kind: "ssl" },
-    { type: "tia.png", stime: 884, etime: 891, kind: "connect" },
-    { type: "tia.png", stime: 891, etime: 893, kind: "wait" },
-
-    { type: "gen_204?v=3&s=webhp&atyp=csi&ei=nWSbV…", stime: 893, etime: 907, kind: "wait" },
-
-    { type: "rs=AA2YrTvRfEtOu2PtNylQ762iZH-GV07GLw", stime: 907, etime: 909, kind: "wait" },
-    { type: "rs=AA2YrTvRfEtOu2PtNylQ762iZH-GV07GLw", stime: 909, etime: 911, kind: "receive" },
-
-    { type: "cb=gapi.loaded_0", stime: 911, etime: 957, kind: "ssl" },
-    { type: "cb=gapi.loaded_0", stime: 957, etime: 1004, kind: "connect" },
-    { type: "cb=gapi.loaded_0", stime: 1004, etime: 1061, kind: "wait" },
-    { type: "cb=gapi.loaded_0", stime: 1061, etime: 1064, kind: "receive" }
+    { key: "http://google.co.kr/", stime: 0, etime: 612, kind: "dns" },
+    { key: "http://google.co.kr/", stime: 612, etime: 613, kind: "connect" },
+    { key: "http://google.co.kr/", stime: 613, etime: 630, kind: "wait" },
+    { key: "http://www.google.co.kr/", stime: 630, etime: 640, kind: "dns" },
+    { key: "http://www.google.co.kr/", stime: 640, etime: 641, kind: "connect" },
+    { key: "http://www.google.co.kr/", stime: 641, etime: 660, kind: "wait" },
+    { key: "https://www.google.co.kr/?gws_rd=ssl", stime: 660, etime: 706, kind: "ssl" },
+    { key: "https://www.google.co.kr/?gws_rd=ssl", stime: 706, etime: 753, kind: "connect" },
+    { key: "https://www.google.co.kr/?gws_rd=ssl", stime: 753, etime: 810, kind: "wait" },
+    { key: "https://www.google.co.kr/?gws_rd=ssl", stime: 810, etime: 813, kind: "receive" },
+    { key: "nav_logo242.png", stime: 813, etime: 829, kind: "wait" },
+    { key: "googlelogo_color_272x92dp.png", stime: 829, etime: 845, kind: "wait" },
+    { key: "i1_1967ca6a.png", stime: 845, etime: 856, kind: "ssl" },
+    { key: "i1_1967ca6a.png", stime: 856, etime: 869, kind: "connect" },
+    { key: "i1_1967ca6a.png", stime: 869, etime: 871, kind: "wait" },
+    { key: "rs=ACT90oHKONBv_Rd-Dj71NZEExnlU9sHrEg", stime: 871, etime: 874, kind: "wait" },
+    { key: "rs=ACT90oHKONBv_Rd-Dj71NZEExnlU9sHrEg", stime: 874, etime: 878, kind: "receive" },
+    { key: "tia.png", stime: 878, etime: 884, kind: "ssl" },
+    { key: "tia.png", stime: 884, etime: 891, kind: "connect" },
+    { key: "tia.png", stime: 891, etime: 893, kind: "wait" },
+    { key: "gen_204?v=3&s=webhp&atyp=csi&ei=nWSbV…", stime: 893, etime: 907, kind: "wait" },
+    { key: "rs=AA2YrTvRfEtOu2PtNylQ762iZH-GV07GLw", stime: 907, etime: 909, kind: "wait" },
+    { key: "rs=AA2YrTvRfEtOu2PtNylQ762iZH-GV07GLw", stime: 909, etime: 911, kind: "receive" },
+    { key: "cb=gapi.loaded_0", stime: 911, etime: 957, kind: "ssl" },
+    { key: "cb=gapi.loaded_0", stime: 957, etime: 1004, kind: "connect" },
+    { key: "cb=gapi.loaded_0", stime: 1004, etime: 1061, kind: "wait" },
+    { key: "cb=gapi.loaded_0", stime: 1061, etime: 1064, kind: "receive" }
 ];
 
 var chart = builder("#chart", {
-    height : 250,
-    padding : {
-        top : 0,
-        bottom : 0,
-        left : 300,
-        right : 0
-    },
+    height : 300,
+    padding : 0,
     theme : "jennifer",
     axis : {
+        padding : {
+            left : 300
+        },
         x : {
             type : "range",
             domain : "etime",
@@ -62,15 +50,20 @@ var chart = builder("#chart", {
         },
         y : {
             domain : getDataToDomain(),
-            hide : true
+            hide : true,
+            format : function(d, i) {
+                return d;
+            }
         },
         data : data
     },
     brush : [{
         type : "timeline",
-        barSize : 7,
+        barSize : function(d, i) {
+            if(d.kind == "dns" || d.kind == "ssl") return 4;
+            return 7;
+        },
         lineWidth : 0,
-        target : [ "stime", "etime" ],
         colors : function(d) {
             if(d.kind == "dns") return "#f699c1";
             else if(d.kind == "ssl") return "#9871b5";
@@ -78,8 +71,24 @@ var chart = builder("#chart", {
             else if(d.kind == "send") return "#f4b355";
             else if(d.kind == "wait") return "#fde448";
             else if(d.kind == "receive") return "#8ac730";
+        },
+        target : [ "kind" ],
+        hideTitle : false
+    }],
+    widget : [{
+        type : "tooltip",
+        format : function(data, key) {
+            return {
+                key: data[key],
+                value: (data.etime - data.stime) + "ms"
+            }
         }
-    }]
+    }],
+    event : {
+        click : function(d) {
+            console.log(d);
+        }
+    }
 });
 
 function getDataToDomain() {
@@ -87,15 +96,13 @@ function getDataToDomain() {
         domain = [ "URL" ];
 
     for(var i = 0; i < data.length; i++) {
-        var type = data[i].type;
+        var key = data[i].key;
 
-        if(!cache[type]) {
-            cache[type] = true;
-            domain.push(type);
+        if(!cache[key]) {
+            cache[key] = true;
+            domain.push(key);
         }
     }
 
     return domain;
 }
-
-console.log(getDataToDomain());
