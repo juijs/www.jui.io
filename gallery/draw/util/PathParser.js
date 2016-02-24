395cca69e319110693aae2d657516e11fa0daba4
easylogic <cyberuls@gmail.com>
2016-02-24 13:56:45 +0900

    update draw modules

--- gallery/draw/util/PathParser.js
+++ gallery/draw/util/PathParser.js
@@ -9,6 +9,19 @@ jui.define("util.parser.path", [], function () {
 				this.parse();
 			}
 
+			this.trim = function (str)  {
+				var arr = str.split(splitReg);
+				var data = [];
+				for (var i = 0, len = arr.length; i < len ; i++ )
+				{
+					if (arr[i] == '') continue;
+
+					data.push(arr[i]);
+				}
+
+				return data; 
+			}
+
 			this.parse = function () {
 				var pathString = pathElement.getAttribute('d');
 				var arr = pathString.match(parseRegForPath);
@@ -18,58 +31,58 @@ jui.define("util.parser.path", [], function () {
 					var segment = arr[i];
 
 					if (segment.indexOf("M") > -1) {
-						var temp = segment.replace("M", "").split(splitReg);
+						var temp = this.trim(segment.replace("M", ""));
 						segments.push({command: "M", values : temp });
 					} else if (segment.indexOf("m") > -1) {
-						var temp = segment.replace("m","").split(splitReg);
+						var temp = this.trim(segment.replace("m",""));
 						segments.push({ command : "m", values : temp });
 					} else if (segment.indexOf("L") > -1) {
-						var temp = segment.replace("L","").split(splitReg);
+						var temp = this.trim(segment.replace("L",""));
 						segments.push({ command : "L", values : temp });
 					} else if (segment.indexOf("l") > -1) {
-						var temp = segment.replace("l","").split(splitReg);
+						var temp = this.trim(segment.replace("l",""));
 						segments.push({ command : "l", values : temp });
 					} else if (segment.indexOf("H") > -1) {
-						var temp = segment.replace("H","").split(splitReg);
+						var temp = this.trim(segment.replace("H",""));
 						segments.push({ command : "H", values : temp });
 					} else if (segment.indexOf("h") > -1) {
-						var temp = segment.replace("h","").split(splitReg);
+						var temp = this.trim(segment.replace("h",""));
 						segments.push({ command : "h", values : temp });
 					} else if (segment.indexOf("V") > -1) {
-						var temp = segment.replace("V","").split(splitReg);
+						var temp = this.trim(segment.replace("V",""));
 						segments.push({ command : "V", values : temp });
 					} else if (segment.indexOf("v") > -1) {
-						var temp = segment.replace("v","").split(splitReg);
+						var temp = this.trim(segment.replace("v",""));
 						segments.push({ command : "v", values : temp });
 					} else if (segment.indexOf("C") > -1) {
-						var temp = segment.replace("C","").split(splitReg);
+						var temp = this.trim(segment.replace("C",""));
 						segments.push({ command : "C", values : temp });
 					} else if (segment.indexOf("c") > -1) {
-						var temp = segment.replace("c","").split(splitReg);
+						var temp = this.trim(segment.replace("c",""));
 						segments.push({ command : "c", values : temp });
 					} else if (segment.indexOf("S") > -1) {
-						var temp = segment.replace("S","").split(splitReg);
+						var temp = this.trim(segment.replace("S",""));
 						segments.push({ command : "S", values : temp });
 					} else if (segment.indexOf("s") > -1) {
-						var temp = segment.replace("s","").split(splitReg);
+						var temp = this.trim(segment.replace("s",""));
 						segments.push({ command : "s", values : temp });
 					} else if (segment.indexOf("Q") > -1) {
-						var temp = segment.replace("Q","").split(splitReg);
+						var temp = this.trim(segment.replace("Q",""));
 						segments.push({ command : "Q", values : temp });
 					} else if (segment.indexOf("q") > -1) {
-						var temp = segment.replace("q","").split(splitReg);
+						var temp = this.trim(segment.replace("q",""));
 						segments.push({ command : "q", values : temp });
 					} else if (segment.indexOf("T") > -1) {
-						var temp = segment.replace("T","").split(splitReg);
+						var temp = this.trim(segment.replace("T",""));
 						segments.push({ command : "T", values : temp });
 					} else if (segment.indexOf("t") > -1) {
-						var temp = segment.replace("t","").split(splitReg);
+						var temp = this.trim(segment.replace("t",""));
 						segments.push({ command : "t", values : temp });
 					} else if (segment.indexOf("A") > -1) {
-						var temp = segment.replace("A","").split(splitReg);
+						var temp = this.trim(segment.replace("A",""));
 						segments.push({ command : "A", values : temp });
 					} else if (segment.indexOf("a") > -1) {
-						var temp = segment.replace("a","").split(splitReg);
+						var temp = this.trim(segment.replace("a",""));
 						segments.push({ command : "a", values : temp });
 					} else if (segment.indexOf("Z") > -1) {
 						segments.push({ command : "Z"});
---
+++ gallery/draw/widget/drawing.mode.pen.js
@@ -0,0 +1,106 @@
+jui.define("util.mode.pen", ["util.parser.path"], function (PathParser) {
+    var DrawingModePen = function (canvas) {
+		
+		var disabled = false; 
+		var parser = new PathParser();
+		var currentPen = [];
+		var events;
+
+		this.init = function () {
+
+		}
+
+		// mode 가 변경될 때 초기 상태로 되돌린다. 
+		this.initMode = function () {
+			var self = this; 
+			// 비활성화 일 때 초기 상태로 되돌림 
+			if (disabled)
+			{
+				if (events) {
+					canvas.offMouseEvent(events);
+				}
+
+			} 
+			// 활성화 모드 
+			else {
+				events = canvas.setMouseEvent( function click(e) { self.dragStart(e); },  function move(e) { self.drag(e); },  function up(e) { self.dragEnd(e); } );
+			}
+		}
+
+		this.drag = function (e) {
+			if (disabled) return; 
+
+			var x = e.clientX - canvas.getDistX();
+			var y = e.clientY - canvas.getDistY();
+
+			currentPen.push( { x : x, y : y });
+			this.drawCurrentPen();
+		}
+
+		this.dragStart = function (e) {
+			if (disabled) return; 
+
+				var x = e.clientX - canvas.getDistX();
+				var y = e.clientY - canvas.getDistY();
+
+				currentPen = [{x: x, y: y}];
+
+				currentPath = canvas.svg.path({
+					className : 'item',
+					fill: 'transparent',
+					stroke: 'black',
+					'stroke-width': 3,
+					'stroke-linejoin': 'round'
+				});
+				canvas.appendToCanvas(currentPath);
+		}
+
+		this.dragEnd = function (e) {
+			if (disabled) return; 
+
+			var x = e.clientX - canvas.getDistX();
+			var y = e.clientY - canvas.getDistY();
+
+			currentPen.push( { x : x, y : y });
+
+			this.drawCurrentPen(true);
+			this.drawEnd();
+		}
+
+		this.drawCurrentPen = function (isEnd) {
+			for(var i = 0, len=  currentPen.length; i < len; i++) {
+				var pen = currentPen[i];
+				if (i == 0) {
+					currentPath.MoveTo(pen.x, pen.y);
+				} else {
+					currentPath.LineTo(pen.x, pen.y);
+				}
+			}
+
+			if (isEnd) {
+				//currentPath.ClosePath();
+			}
+			currentPath.join(true);
+		}
+
+		this.drawEnd = function () {
+
+			currentPath = null;
+			currentPen = [];
+		}
+
+		this.setDisabled = function (value) {
+			disabled = value;
+
+			this.initMode();
+		}
+
+		this.getDisabled = function () {
+			return !!disabled;
+		}
+  
+    };
+
+
+    return DrawingModePen;
+});
