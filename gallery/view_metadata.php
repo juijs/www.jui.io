48d611aab52146aac6497b5138dd00fc491cc848
seogi1004 <seogi777@gmail.com>
2016-04-02 02:26:52 +0900

    Project title changed  : JENNIFER UI -> JUI Framework

--- gallery/view_metadata.php
+++ gallery/view_metadata.php
@@ -6,12 +6,12 @@
 	$isThumbnail = file_exists("gallery/".$id."/thumbnail.png");
 ?>
 
-<title>JENNIFER Gallery: <?php echo $json->title ?></title>
-<meta name="title" content="JENNIFER UI: <?php echo $json->title ?>" />
+<title>JUI Gallery: <?php echo $json->title ?></title>
+<meta name="title" content="JUI Framework: <?php echo $json->title ?>" />
 <meta name="description" content="<?php echo $json->description ?>" />
 <meta name="keywords" content="<?php echo join(",", $json->keywords) ?>" />
 <meta name="author" content="<?php echo $json->author ?>" />
-<meta property="og:title" content="JENNIFER UI: <?php echo $json->title ?>" />
+<meta property="og:title" content="JUI Framework: <?php echo $json->title ?>" />
 <meta property="og:description" content="<?php echo $json->description ?>" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="http://jui.io/?p=gallery.<?php echo $json->name ?>" />
